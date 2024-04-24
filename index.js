85214c8f914510a78b7eefaa5f979498709a590f
Harmit Sidhu <harmit_sidhu@outlook.com>
2024-04-24 09:54:46 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import warehousesRouter from './routes/warehousesRoutes.js';
+import inventoriesRouter from './routes/inventoriesRoutes.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use('/warehouses', warehousesRouter);
+app.use('/inventories', inventoriesRouter);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ routes/inventoriesRoutes.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import fs from 'fs';
+import { v4 as uuidv4 } from 'uuid';
+
+
+
+
+export default router;
