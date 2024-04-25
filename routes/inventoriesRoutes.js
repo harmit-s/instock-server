ae0c0973c5721ebff77a90047c8ccd70e88eb24c
yigit ocak <yigitockk@gmail.com>
2024-04-24 23:00:54 -0400

    created a route for deletion of a inventory item

--- routes/inventoriesRoutes.js
+++ routes/inventoriesRoutes.js
@@ -2,7 +2,22 @@ import express from 'express';
 import fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
 const router = express.Router()
+import knex from 'knex';
+import knexfile from '../knexfile.js';
+const db = knex(knexfile.development);
 
+router.delete("/:id", async (req, res) => {
+    const id = req.params.id
+    try {
+        const itemData = await db('inventories')
+            .where({id: id})
+            .del();
 
+        res.status(204).json("")
+    }
+    catch (err) {
+        res.status(404).json({message: "No Inventory Item Found!"})
+    }
+})
 
 export default router;
